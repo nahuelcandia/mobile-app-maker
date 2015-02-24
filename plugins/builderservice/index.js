5fc8398f99338420704ea3189f3ef7bd397cfbf2
osk <oskosk@gmail.com>
2015-02-24 01:38:08 -0300

    changed hook render for beforerender and afterrender

--- plugins/builderservice/index.js
+++ plugins/builderservice/index.js
@@ -136,13 +136,13 @@ function builder(app, server, sockets) {
 }
 
 builder.renderAndRequestBuild = function(app, clientSocket) {
-  app.cms.frontend.trigger("render", {}, {}, function(err, data, locals) {
+  app.cms.frontend.render(app, {}, function(err, html) {
     if (err) {
       clientSocket.emit("error rendering frontend");
       debug("Rendering failed on builder %s", err.stack);
       return;
     }
-    var zipStream = packager.createAppZipStream(data.html);
+    var zipStream = packager.createAppZipStream(html);
     //temporary file for getting the stream size
     var filename = path.join(process.cwd(), "filestorage", "tmp", "app.zip");
     var fs = require("fs");
