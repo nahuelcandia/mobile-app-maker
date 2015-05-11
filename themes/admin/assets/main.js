0542eea698d4c1d6fadcba78e3a96bc5d1003392
Nahuel Candia <home@Nahuel-Candia.local>
2015-05-10 19:19:07 -0700

    Frontend improvements

--- themes/admin/assets/main.js
+++ themes/admin/assets/main.js
@@ -37,11 +37,9 @@
  * (info@shovelapps.com) / www.shovelapps.com / www.shovelapps.org
  */
 
-$(function() {
+ $(function() {
   $(document).on("click", ".dropdown.keep-open .dropdown-menu", function(e) {
-    e.preventDefault();
     e.stopPropagation();
-    return false;
   });
 
   $(document).on("click", ".shovel-group-panel .panel-heading[data-toggle]", function(e) {
@@ -53,10 +51,140 @@ $(function() {
 			$('.select2').select2();
 		}*/
 
-  $(document).on("click", ".up-arrow", function() {
-    $("html, body").animate({
-      scrollTop: 0
+    $(document).on("click", ".up-arrow", function() {
+      $("html, body").animate({
+        scrollTop: 0
+      });
     });
-  });
 
-});
+    $(document).on("change", ".information-build .addscreen input[type='file']", function(){
+      if(!("files" in this) ||  this.files.length == 0 || URL === undefined)
+        return;
+
+      if(this.files[0].type.indexOf("image/") == 0)
+      {
+        $(this).find("~ img").remove(), 
+        $(this).after($("<img>").attr("src", URL.createObjectURL(this.files[0])));
+      }
+    });
+
+    $(document).on("change", ".dropdown-menu .attach input[type='file']", function(){
+      if("files" in this && this.files.length == 0)
+        return;
+
+      var filename = "files" in this ? this.files[0].name : $(this).val().split('\\').pop();
+      $(this).find("~ span:first").text(filename);
+    });
+
+    $(document).on("change", ".cms.settingsapp .attach-icon input[type='file']", function(){
+      if(!("files" in this) ||  this.files.length == 0 || URL === undefined)
+        return;
+
+      if(this.files[0].type.indexOf("image/") == 0)
+      {
+        $(this).find("~ img").remove(), 
+        $(this).after($("<img>").attr("src", URL.createObjectURL(this.files[0])));
+      }
+    });
+
+    $(document).on("click", ".list-selects .header input[type='checkbox']", function(){
+      $(this).parents(".list-selects").find("li input[type='checkbox']").prop("checked", $(this).is(':checked'));
+    });
+
+    $(document).on("click", ".list-selects .list li input[type='checkbox']", function(){
+      var checks = $(this).parents(".list").find("li input[type='checkbox']");
+      var count = 0;
+
+      for(var i = 0; i < checks.length; i++)
+      {
+        checks.eq(i).is(':checked') && ++count;
+      }
+
+      $(this).parents(".list-selects").find(".header input[type='checkbox']:first").prop("checked", count == checks.length);
+    });
+
+
+
+
+    // actions
+
+
+    $(".publish-history-table").length > 0 && (function(){
+      $(document).on("click", ".publish-history-table .delete", function(){
+          var parent = $(this).parents("tr");
+          parent.fadeOut(function(){parent.remove();});
+          return false;
+      });
+    })();
+
+    window.history.length > 1 && $(".back-button").length > 0 && (function(){
+      $(".back-button").on("click", function(){ window.history.back(); return false;});
+    })();
+    window.history.length == 1 && (function(){
+      $(".back-button").hide();
+    })();
+
+
+    $(".plugins").length > 0 && (function(){
+      $(".search input").on("keyup", function(){
+        var val = $(this).val().toLocaleLowerCase().trim();
+
+        if(val == "")
+        {
+          $(".plugins > div").fadeIn();
+          return;
+        }
+        $(".plugins > div").each(function(i){
+          if($(this).find(".ellipsis").text().toLocaleLowerCase().indexOf(val) == -1)
+            $(this).stop().fadeOut();
+          else
+            $(this).stop().fadeIn();
+        });
+      });
+    })();
+
+    $(".categories").length > 0 && (function(){
+
+      $(".search input").on("keyup", function(){
+        var val = $(this).val().toLocaleLowerCase().trim();
+
+        if(val == "")
+        {
+          $(".categories .categorie, .screens, .screens .screen").fadeIn();
+          return;
+        }
+        $(".categories .categorie").each(function(k){
+          var count = 0, screens = $(this).next(".screens:first"), elements = screens.find(".screen");
+          for(var i in elements)
+          {
+            var element = elements.eq(i), txt = element.text().trim().toLocaleLowerCase();
+            if(txt.indexOf(val) == -1)
+            {
+              element.fadeOut();
+            }
+            else
+            {
+              element.fadeIn();
+              count++;
+            }
+          }
+
+          if(count == 0)
+          {
+            screens.fadeOut();
+            $(this).stop().fadeOut();
+          }
+          else
+          {
+            screens.fadeIn();
+            $(this).stop().fadeIn();
+          }
+
+        });
+      });
+    })();
+
+
+
+
+  });
